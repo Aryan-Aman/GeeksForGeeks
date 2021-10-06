f6e032a671484e4d20e7621e166c6f8a3cab645e
Aman <aman209001@gmail.com>
2021-10-06 07:56:42 +0530

    Create Bitwise Operators.py

---
+++ Fork_Python/Bitwise Operators.py
@@ -0,0 +1,40 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+
+def bitwise(a, b, c):
+    d=a^a
+    print(d)
+    e=c^b
+    print(e)
+    f=a&b
+    print(f)
+    g=c|(a^a)
+    print(g)
+    e= ~e
+    print(e)
+  
+
+#{ 
+#Driver Code Starts.
+
+
+
+def main():
+    testcases=int(input()) #testcases
+    while(testcases>0):
+        a = int(input())
+        b = int(input())
+        c = int(input())
+        bitwise(a, b, c)
+        testcases-=1
+        
+
+
+if __name__=='__main__':
+    main()
+#} Driver Code Ends
