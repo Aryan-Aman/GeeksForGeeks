4c2e27cf8552bb0d78bbed03d792b7ae2c0a5dd2
Aman <aman209001@gmail.com>
2021-10-06 11:47:31 +0530

    Create JumpingThroughWhile.py

---
+++ Fork_Python/JumpingThroughWhile.py
@@ -0,0 +1,9 @@
+def printIncreasingPower(x):
+    ##Your code here
+    i=1
+    # Loop to jump in powers of 2
+    while(i*i<x+1):
+        ##Your code here
+        
+        print (i*i , end = " ")
+        i+=1
