301b3b4b1f2d6f63eb643b2bbb5182cca5dfc25f
Aman <aman209001@gmail.com>
2021-10-06 11:57:46 +0530

    Create zero_converter.py

---
+++ Fork_Python/zero_converter.py
@@ -0,0 +1,11 @@
+def pos(n):
+   i=n-1
+   while(i>=0):
+       print(i,end=" ")
+       i-=1## Write the code
+        
+    
+def neg(n):
+   while(n<=0):
+       print(n,end=" ")
+       n+=1##Write the code
